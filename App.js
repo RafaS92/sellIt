472b1c7e36da35ff2d6609d4fb43717d67f8aac1
RafaS92 <manrvaldez..92@gmail.com>
2020-10-24 10:12:50 -0500

    text testing

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  SafeAreaSafeAreaView,
+} from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed");
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} onPress={handlePress()}>
+        {" "}
+        papa lorem100phi hfgiph spgpi ship hip hpivsidp' fiihg hipvafphipsg ipsh
+        g jkeksafkash'gf hahd ha ihpdahs gfh psih ghxf gphx fgihp{" "}
+      </Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "dodgerblue",
   },
 });
