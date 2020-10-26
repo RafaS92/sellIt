b5abca9ebd75aaf229c12d803d9fbd099c37d8cf
RafaS92 <manrvaldez..92@gmail.com>
2020-10-26 10:46:00 -0500

    image picker working

---
+++ app/components/snippets.js
@@ -0,0 +1,17 @@
+import { View,StyleSheet } from 'react-native'
+import styles from '../config/styles'
+
+
+export default function other() {
+    return (
+        <View style={styles.container}>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+})
