e9cb851bfc90530d9028a80d9851c91980740117
RafaS92 <manrvaldez..92@gmail.com>
2020-10-27 17:27:41 -0500

    listing pull from server

--- app/api/client.js
+++ app/api/client.js
@@ -2,7 +2,7 @@ import {create} from 'apisauce'
 
 
 const apiClient = create({
-    baseURL:'"http:/10.0.0.26:9000/assets/"'
+    baseURL:'http:/10.0.0.26:9000/api'
 })
 
 export default apiClient
--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import Screen from "../components/Screen";
 import Card from "../components/Card";
 import { StyleSheet, FlatList } from "react-native";
@@ -6,32 +6,43 @@ import { StyleSheet, FlatList } from "react-native";
 import colors from "../config/colors";
 import navigationTheme from "../navigation/navigationTheme";
 import routes from "../navigation/routes"
+import listingsApi from "../api/listings"
 
-const listings = [
-  {
-    id: 1,
-    title: "shoes for sale",
-    image: require("../assets/hermes.png"),
-  },
-  {
-    id: 2,
-    title: "dogsfor sale",
-    image: require("../assets/Rafablack.png"),
-  },
-];
+
 
 function ListingsScreen({navigation}) {
+
+
+const [listings,setListings ]= useState([])
+
+
+useEffect(() => {
+
+  loadListings()
+  console.log()
+ 
+}, [])
+
+const loadListings = async () => {
+const response = await listingsApi.getListings()
+setListings(response.data)
+
+}
+
+
+
   return (
     <Screen style={styles.screen}>
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
         renderItem={({ item }) => (
+          
           <Card
-            title={item.title}
-            subTitle={"$" + item.price}
-            image={item.image}
-            onPress={()=>  navigation.navigate(routes.LISTING_DETAILS,item)}
+          title={item.title}
+          subTitle={"$" + item.price}
+          imageUrl={item.images[0].url}
+          onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
       />
