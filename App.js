8f6dfc6a4a067f90fd18faf1d9841929f685d3a8
RafaS92 <manrvaldez..92@gmail.com>
2020-10-27 07:11:57 -0500

    Solved error of 6hrs

--- App.js
+++ App.js
@@ -25,18 +25,71 @@ import ListingEditScreen from './app/screens/ListingEditScreen';
 import ImageInput from './app/components/ImageInput';
 import ImageInputList from './app/components/ImageInputList';
 
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer,useNavigation } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+
+const Link =() => {
+
+ const navigation = useNavigation()
+  
+  return(
+    <Button 
+    title="Click"
+    onPress={() => navigation.navigate("TweetDetails",{id: 1})}
+    />
+
+  )
+  
+
+}
+
+const Tweets = ({navigation})=>(
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+) 
+
+const TweetDetails = ({route}) => (
+  <Screen>
+    <Text>TweetDetails {route.params.id}</Text>
+  </Screen>
+) 
+
+const Stack = createStackNavigator()
+
+const StackNavigator = () =>(
+  <Stack.Navigator
+    screenOptions={{
+      headerStyle:{backgroundColor: "blue"},headerTintColor: "white"
+
+    }}
+  >
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} options={{headerStyle:{backgroundColor: "gold"},headerTintColor: "white", headerShown: false}} />
+  </Stack.Navigator>
+)
+
+const Account = () => <Screen><Text>Account</Text></Screen>
+
+const Tab = createBottomTabNavigator()
+const TabNavigator = () =>(
+  <Tab.Navigator>
+    <Tab.Screen  name="Feed" component={Tweets}  options={{tabBarIcon: ({size,color}) => <MaterialCommunityIcons name="home" size={size} color={color} /> }} />
+    <Tab.Screen name="Account" component={Account} />
+  </Tab.Navigator>
+)
  
 export default function App() {
 
-  
   return (
 
-    
-
-    <Screen>
+    <NavigationContainer>
+      <TabNavigator />
+    </NavigationContainer>
      
-      <ListingEditScreen  />
-    </Screen>
+    
    );
 }
