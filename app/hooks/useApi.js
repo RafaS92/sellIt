5827298ab091272a2c2251579e4c1b87f76fb096
RafaS92 <manrvaldez..92@gmail.com>
2020-10-27 19:49:04 -0500

    submiting a new listing structure

--- app/hooks/useApi.js
+++ app/hooks/useApi.js
@@ -1,31 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
+export default useApi = (apiFunc) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const request = async (...args) => {
+    setLoading(true);
+    const response = await apiFunc(...args);
+    setLoading(false);
 
-export default useApi = (apiFunc) =>{
+    if (!response.ok) return setError(true);
 
-const [data,setData]= useState([])
-const [error,setError] = useState(false)
-const [loading,setLoading] = useState(false)
+    setError(false);
+    setData(response.data);
+  };
 
-
-const request = async (...args) => {
-    setLoading(true)
-    const response = await apiFunc(...args)
-    setLoading(false)
-    
-    if(!response.ok) return setError(true)
-    setError(false)
-    setData(response.data)
-    }
-
-    return{ data,error,loading,request}
-    
-
-
-
-
-
-
-
-}
+  return { data, error, loading, request };
+};
